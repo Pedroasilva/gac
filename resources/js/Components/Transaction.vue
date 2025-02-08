b1804b827adb4ff180691d754172fdd7457760b0
Pedro Augusto <pedro.silva@vortigo.com.br>
2025-02-08 16:37:45 -0300

    atualiza componente de transações, ajusta layout do dashboard e modifica estilo do formulário de edição de perfil

--- resources/js/Components/Transaction.vue
+++ resources/js/Components/Transaction.vue
@@ -31,20 +31,22 @@ export default {
 </script>
 
 <template>
-    <div class="bg-gray-200 p-2 mb-2 rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center hover:bg-gray-300">
+    <div
+        class="bg-gray-200 p-2 mb-2 rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center hover:bg-gray-300">
         <div class="mb-2 md:mb-0">
             <p class="text-xs text-gray-600">{{ date }}</p>
-            <p :class="{'text-red-600': type === 'sended', 'text-green-600': type === 'received'}" class="text-xl">{{ description }} {{ amount }}</p>
+            <p :class="{ 'text-red-600': type === 'out', 'text-green-600': type === 'in' }" class="text-xl">
+                {{ amount }} -
+                {{ description }}
+            </p>
         </div>
         <div class="flex space-x-2">
-            <button
-                class="bg-blue-600 text-white px-3 py-1 rounded-lg hover:bg-blue-800 transition duration-300"
+            <button class="bg-blue-600 text-white text-sm px-2 py-1 rounded-lg hover:bg-blue-800 transition duration-300"
                 @click="handleReceipt">
                 <i class="fa-solid fa-receipt"></i>
                 Receipt
             </button>
-            <button
-                class="bg-red-600 text-white px-3 py-1 rounded-lg hover:bg-red-800 transition duration-300"
+            <button class="bg-red-600 text-white text-sm px-2 py-1 rounded-lg hover:bg-red-800 transition duration-300"
                 @click="handleReverse">
                 <i class="fa-solid fa-rotate-left"></i>
                 Reverse
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,10 +9,10 @@ import Transaction from '@/Components/Transaction.vue';
     <Head title="Account" />
     <AuthenticatedLayout>
 
-        <div class="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-8 mt-5">
+        <div class="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-8 mt-3">
             <div class="text-center mb-8">
                 <h1 class="text-3xl font-bold">Total Balance</h1>
-                <p class="text-4xl text-green-600 font-semibold mt-2">$2,562.50</p>
+                <p class="text-4xl text-green-600 font-semibold mt-2">$ 2,562.50</p>
             </div>
 
             <div class="flex justify-center space-x-6 mb-8">
@@ -29,23 +29,29 @@ import Transaction from '@/Components/Transaction.vue';
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
                 <div class="bg-gray-200 p-6 rounded-lg">
                     <h2 class="text-xl font-semibold">Income</h2>
-                    <p class="text-2xl text-green-600 mt-2">$552.95</p>
+                    <p class="text-2xl text-green-600 mt-2">$ 552.95</p>
                 </div>
                 <div class="bg-gray-200 p-6 rounded-lg">
                     <h2 class="text-xl font-semibold">Expenses</h2>
-                    <p class="text-2xl text-red-600 mt-2">$86.45</p>
+                    <p class="text-2xl text-red-600 mt-2">$ 86.45</p>
                 </div>
             </div>
 
-            <div class="mb-8">
+            <div class="mb-6">
                 <h2 class="text-xl font-semibold mb-4">Transactions</h2>
-                <Transaction date="2021-10-01 12:45:23" description="Payment from client" amount="$500.00"
-                    type="sended" />
+                <Transaction date="2021-10-01 12:45:23" description="Transfer to client" amount="$ 500.00" type="out" />
+                <Transaction date="2021-10-01 12:45:23" description="Received from client" amount="$ 23.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 500.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 50.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 121.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 35.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 87.00" type="in" />
+                <Transaction date="2021-10-01 12:45:23" description="Deposit" amount="$ 15,550.00" type="in" />
             </div>
 
             <button
-                class="w-full bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">View
-                All</button>
+                class="w-full bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">Load
+                more</button>
         </div>
 
     </AuthenticatedLayout>
