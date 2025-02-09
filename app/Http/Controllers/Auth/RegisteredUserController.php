07f99b258deefa432a9922ca1b12c4752bd7acf2
Pedro Augusto <pedro.silva@vortigo.com.br>
2025-02-08 23:02:20 -0300

    adiciona serviço de conta bancária e ajusta registro de usuário para criar conta automaticamente

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UserStoreRequest;
 use App\Models\User;
+use App\Services\BankAccountService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
+    protected $bankAccountService;
+
+    public function __construct(BankAccountService $bankAccountService)
+    {
+        $this->bankAccountService = $bankAccountService;
+    }
+
     /**
      * Display the registration view.
      */
@@ -28,14 +35,8 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(UserStoreRequest $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -45,6 +46,8 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        $this->bankAccountService->createAccount($user);
 
         return redirect(route('dashboard', absolute: false));
     }
--- app/Services/BankAccountService.php
+++ app/Services/BankAccountService.php
@@ -3,9 +3,31 @@
 namespace App\Services;
 
 use App\Models\BankAccount;
+use App\Models\User;
 
 class BankAccountService
 {
+    public function createAccount(User $user)
+    {
+        BankAccount::create([
+            'user_id' => $user->id,
+            'account_number' => $this->generateAccountNumber(),
+            'agency' => $this->generateAgencyNumber(),
+            'balance' => 0,
+            'account_type' => 'wallet',
+        ]);
+    }
+
+    private function generateAgencyNumber()
+    {
+        return rand(1000, 9999);
+    }
+
+    private function generateAccountNumber()
+    {
+        return rand(1000000000, 9999999999);
+    }
+
     public function deposit(BankAccount $account, $amount)
     {
         $account->balance += $amount;
